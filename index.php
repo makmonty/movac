7ddd0d6732159eeabb480898c73c8001b77e6b60
Ángel Brasero Moreno <angel.brasero@gmail.com>
2016-04-15 00:45:23 +0200

    Use path relative to project

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ require_once(__DIR__ . "/core/init.php");
 use \plugin\Plugin as Plugin;
 
 Plugin::activate("doctrine1");
-Plugin::activate("mvc", __DIR__ . "/vendor/makmonty/movac-mvc");
+Plugin::activate("mvc", dirname($_SERVER["SCRIPT_FILENAME"]) . "/vendor/makmonty/movac-mvc");
 // Plugin::activate("mvc", __DIR__ . "/../movac-mvc");
 
 Plugin::activate("jquery");
